0c973a0a78d2b5328419bafecb10b94b91a27d4e
Stephanie Li <stephanieli@Stephanies-MacBook-Air.local>
2022-02-06 05:21:47 -0500

    shorten style of hour hand

--- public/script.js
+++ public/script.js
@@ -5,6 +5,7 @@ window.addEventListener("load", () => {
 
   const setDate = () => {
     const now = new Date();
+    console.log("this is now--->", now);
     const seconds = now.getSeconds();
     const minutes = now.getMinutes();
     const hours = now.getHours();
@@ -13,7 +14,18 @@ window.addEventListener("load", () => {
     // console.log(minutes);
     // console.log(hours);
     const secondsDegree = (seconds / 60) * 360 + 90;
+    // console.log("second--->", secondsDegree);
     secondHand.style.transform = `rotate(${secondsDegree}deg)`;
+
+    // minutes
+    // console.log("minnutteee-->", minutes); // 48
+    const minutesDegree = (minutes / 60) * 360 + 90;
+    // console.log("this is minuteDegree", minutesDegree);
+    minuteHand.style.transform = `rotate(${minutesDegree}deg)`;
+
+    // hours
+    const hoursDegree = (hours / 12) * 360 + 90;
+    hourHand.style.transform = `rotate(${hoursDegree}deg)`;
   };
 
   setInterval(setDate, 1000);
