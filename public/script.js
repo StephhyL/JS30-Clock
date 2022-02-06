673a9a654dd871b9ad766abccafd1431a28081bd
Stephanie Li <stephanieli@Stephanies-MacBook-Air.local>
2022-02-06 04:38:17 -0500

    query select minutes and hours

--- public/script.js
+++ public/script.js
@@ -1,11 +1,18 @@
 window.addEventListener("load", () => {
   const secondHand = document.querySelector(".second-hand");
+  const minuteHand = document.querySelector(".min-hand");
+  const hourHand = document.querySelector(".hour-hand");
 
   const setDate = () => {
     const now = new Date();
     const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
+
     // console.log(seconds);
-    const secondsDegree = (seconds / 60) * 360;
+    // console.log(minutes);
+    // console.log(hours);
+    const secondsDegree = (seconds / 60) * 360 + 90;
     secondHand.style.transform = `rotate(${secondsDegree}deg)`;
   };
 
