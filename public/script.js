b62b4f64edf817b627944f7eadbc2bef509886cd
Stephanie Li <stephanieli@Stephanies-MacBook-Air.local>
2022-02-06 04:34:33 -0500

    add transform to seconds hand

--- public/script.js
+++ public/script.js
@@ -1,6 +1,12 @@
 window.addEventListener("load", () => {
+  const secondHand = document.querySelector(".second-hand");
+
   const setDate = () => {
-    console.log("hi");
+    const now = new Date();
+    const seconds = now.getSeconds();
+    // console.log(seconds);
+    const secondsDegree = (seconds / 60) * 360;
+    secondHand.style.transform = `rotate(${secondsDegree}deg)`;
   };
 
   setInterval(setDate, 1000);
